3bcb80b5491f5cbe0a19f96caea6b3c23c2113ca
Cynthia <cynkrg157@gmail.com>
2020-01-27 15:00:19 -0600

    Separacion de clases por ingesta de archivos y stream

---
+++ src/main/java/com/richit/GetValidations.java
@@ -0,0 +1,117 @@
+package com.richit;
+
+import org.apache.commons.lang.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GetValidations {
+    //Metodo para encontrar el delimitador en los Header
+    public static String FindDelimH(String header){
+        int i, cequal=0;
+        String aux = "", result="";
+        String [] listd = {",",";","|"," ","\t","-"};
+        Object [][] matrixd = new Object[listd.length][3];
+
+        //Se llena la matriz
+        for(i=0;i<listd.length;i++){
+            matrixd[i][0]=listd[i];
+            matrixd[i][1]=0;
+            matrixd[i][2]=listd.length-i;
+        }
+
+        for (i=0; i<listd.length;i++) matrixd[i][1] = StringUtils.countMatches(header, listd[i]);
+
+        for (i=0;i<listd.length;i++){
+            if((Integer)matrixd[i][1]!=0){
+                cequal++;
+                aux = (String) matrixd[i][0];
+                break;
+            }
+        }
+        if (cequal==0) result="1 campo";
+        else if (cequal>=1) result=aux;
+
+        return result;
+    }
+
+    //Metodo para encontrar el delimitador en los datos
+    public static String FindDelimD(String stream, int fields){
+        int i, cmax=0,cmin=0,cequal=0,a=0,b=0;
+        String dd = "";
+
+        //Lista de delimitadores
+        String [] listd = {",",";","|"," ","\t","-"};
+
+        //Matriz de delimitadores, cantidad y jerarquía
+        Object [][] matrixd = new Object[listd.length][3];
+        for(i=0;i<listd.length;i++){
+            matrixd[i][0]=listd[i];
+            matrixd[i][1]=0;
+            matrixd[i][2]=listd.length-i;
+        }
+
+        //Guardar en un arreglo las veces que se repite cada valor de los delimitadores
+        for (i=0; i<listd.length;i++) matrixd[i][1] = StringUtils.countMatches(stream, listd[i]);
+        //Cuantos delimitadores coinciden con los campos o no
+        for (i=0;i<listd.length;i++){
+            if ((Integer)matrixd[i][1] > fields) cmax++; //Mas campos de los esperados
+            else if ((Integer)matrixd[i][1]<fields && (Integer)matrixd[i][1]!=0) cmin++; //Menos campos de los esperados
+            else if ((Integer)matrixd[i][1]==fields) cequal++; //El delimitador se presenta las mismas veces que el número de campos
+        }
+
+        if(cmax==0){
+            //Existe uno o mas delimitadores que coinciden con los campos (Caso 4)
+            if (cmin==0 && cequal>0) {
+                for (i=0;i<listd.length;i++) {
+                    if ((Integer) matrixd[i][1] == fields) {
+                        dd = (String) matrixd[i][0];
+                        break;
+                    }}}
+            //Existen menos delimitadores que coincidan con los campos (Caso 5)
+            //No existe ningún delimitador que coincida con los campos (Caso 3)
+            else if (cequal==0 && cmin>=0) dd="1";
+                //Existen uno o más delimitadores que coincidan con los campos y uno o más que son menos a los campos(Caso 7)
+            else if (cmin>0 && cequal>0) {
+                for (i = 0; i < listd.length; i++) {
+                    if ((Integer) matrixd[i][1] == fields) {
+                        a = (Integer) matrixd[i][2];
+                        dd = (String) matrixd[i][0];
+                    } else if ((Integer) matrixd[i][1] < fields && (Integer) matrixd[i][1] > 0)
+                        b = (Integer) matrixd[i][2];
+                }
+                if (a <= b) dd="1";
+            }}
+        else{
+            //Existen más delimitadores que los campos (Caso 6)
+            //Existen más delimitadores que los campos y al mismo tiempo menos que los campos (Caso 8)
+            if (cequal==0) dd="2";
+                //Existen más delimitadores que los campos, a su vez menos delimitadores que los campos y uno o más delimitadores iguales a los campos(Caso 1)
+                //Existen más delimitadores que los campos y a la vez uno o más delimitadores iguales a los campos (Caso 2)
+            else if (cequal>0) {
+                for (i = 0; i < listd.length; i++) {
+                    if ((Integer) matrixd[i][1] == fields && fields!=0) {
+                        a = (Integer) matrixd[i][2];
+                        dd = (String) matrixd[i][0];
+                    } else if ((Integer) matrixd[i][1] > fields) b = (Integer) matrixd[i][2];
+                }
+                if (a <= b) dd="2";
+            }}
+
+        return dd;
+    }
+
+    //Metodo para validar JSON
+    public static boolean isJson(String Json) {
+        try {
+            new JSONObject(Json);
+        } catch (JSONException ex) {
+            try {
+                new JSONArray(Json);
+            } catch (JSONException ex1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/richit/InputStream.java
@@ -0,0 +1,60 @@
+package com.richit;
+
+import org.apache.commons.lang.StringUtils;
+import org.json.CDL;
+import org.json.JSONArray;
+
+import static com.richit.GetValidations.*;
+
+public class InputStream {
+    public static void main(String[] args) {
+
+        String header = "Col1,Col2,Col3,Col4";
+        //String header = "Column1";
+        String dh = FindDelimH(header);
+        String dd;
+
+        //Cuantas veces el delimitador se encuentra en el header
+        int fields = StringUtils.countMatches(header, dh);
+
+        //Pruebas de varias entradas de datos
+        String ex1 = "datoA,datoB";                                 //Menos campos
+        String ex2 = "a,b,c,d,e,f,g";                               //Mas campos
+        String ex3 = "dato1,dato2,dato3,dato4";                     //Coincide
+        String ex4 = "d1;d2;d3;d4";                                 //Coincide
+        String ex5 = "a b c d";                                     //Coincide
+        String ex6 = "1|2|3|4";                                     //Coincide
+        String ex7 = ",,,1";                                        //Coincide
+        String ex8 = "as\tdc\ted\tqw";                              //Coincide
+        String ex9 = "data1,data2-a,data2-b,data3";                 //Coincide, incluye guiones
+        String ex10 = "1-a,1-b,1-c,1-d";                            //Coincide, incluye comas y guiones pero el delimitador es por comas
+        String ex11 = "a-1,a-2,a-3,b";                              //Coincide, misma cantidad de guiones y comas
+        String ex12 = "q-1,q-2,q-3,a4,dw,sds";                      //No coincide, incluye guiones y comas
+        String ex13 = "a-b-c,d-1";                                  //No coincide, incluye comas y guiones
+        String ex14 = "a-b-c-d-e,1-e,2-e,3";                        //Coincide
+        String ex15 = "abc dfdf de la defd;24;col ssjd;del-jdjd";   //Coincide (jerarquía)
+        String ex16 = "a";                                          //Menos campos
+
+        String data = ex15;
+
+        //Regresa delimitador, un 1 si son menos campos y un 2 si son más campos
+        dd = FindDelimD(data, fields);
+
+        //Convierte a JSON si coincide
+        if (dd == "1") System.out.println("Se tienen menos campos");        //Enviar a tópico de error
+        else if (dd == "2") System.out.println("Se tienen mas campos");     //Enviar a tópico de error
+        else {
+            if (fields == 0) { //Toma el caso de un campo
+                String fc = header + "\n" + data;
+                JSONArray array = CDL.toJSONArray(fc);
+                System.out.println(array.toString(2));
+            } else {
+                //System.out.println("El delimitador es \"" + dd + "\"");
+                if (dd != ",") data = data.replace(dd, ",");
+                String fc = header + "\n" + data;
+                JSONArray array = CDL.toJSONArray(fc);
+                System.out.println(array.toString(2));
+            }
+        }
+    }
+}
